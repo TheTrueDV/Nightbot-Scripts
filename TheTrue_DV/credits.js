19373b93d09cbeef85f97c8aaf046d9300a7e8d0
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2021-04-25 15:31:53 +0100

    Update

--- TheTrue_DV/credits.js
+++ TheTrue_DV/credits.js
@@ -1,12 +1,15 @@
 var response = false;
 var creditList = ['Yeti', 'Commands'];
-switch ('$(q)'.toLowerCase()) {
+switch ($(q).toLowerCase()) {
     case 'yeti':
         response = 'Check out the adorable Kopi at https://twitter.com/Kopintea';
         break;
     case 'commands':
         response = 'Check out the nerdy True at https://twitter.com/TheTrue_DV';
         break;
+    case '':
+        response = 'No filter selected.';
+        break;
     default:
-        response = 'Did you forget to filter? Try !credit Commands! Valid filters: ' + creditList.join(", ").toLowerCase();
+        response = 'Did you forget to filter? Try !credit Commands! Valid filters: ' + creditList.join(", ").toLowerCase() + '$(q)'.toLowerCase() ;
 }
