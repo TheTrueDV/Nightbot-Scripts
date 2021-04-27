40cd49ed5123763eb98271d67ea3f2f78bfbba27
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2021-04-27 02:06:54 +0100

    Temp

---
+++ AriaASMR/credits.js
@@ -0,0 +1,12 @@
+var response = false;
+var creditList = ['Yeti', 'Commands'];
+switch (q.toLowerCase()) {
+    case 'yeti':
+        response = 'Check out the adorable Kopi at https://twitter.com/Kopintea';
+        break;
+    case 'commands':
+        response = 'Check out the nerdy True at https://twitter.com/TheTrue_DV';
+        break;    
+    default:
+        response = 'Did you forget to filter? Try !credit Commands! Valid filters: ' + creditList.join(", ");
+}
