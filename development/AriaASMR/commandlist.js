c246ac842511a55cae64cd1c16fbff3810f282b5
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2021-04-28 23:37:29 +0100

    Fixed typo

--- development/AriaASMR/commandlist.js
+++ development/AriaASMR/commandlist.js
@@ -1,4 +1,4 @@
 var response = false;
 var creditList = ['Credits', 'Disruption', 'Mics'];
 
-response = 'The custom commands DV has implemented so far are: ' + creditList.join(", ");
+response = 'The custom commands DV has implemented so far are: ' + creditList.join(", ") + '. Try "!commands" for the rest.';
