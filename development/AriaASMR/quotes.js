e3d4326ad7789e7e97e006db32f00251655fa26d
thetruedv <matthewjdavison1+dv@outlook.com>
2021-05-02 00:33:41 +0100

    Updated Quote

--- development/AriaASMR/quotes.js
+++ development/AriaASMR/quotes.js
@@ -2,7 +2,7 @@ var response = false;
 var quotes = ['vivi'];
 switch (q.toLowerCase()) {
     case 'vivi':
-        response = 'I think that getting, getting jealous or worried that spreading your community around means that you\'ll get less community, that\'s such a dumb thing. That\'s such a dumb thing to - like, I understand the need jerk worry maybe, but I also think it\'s a pretty toxic mindset to have, because it means that you\'re holding on too hard. You\'re like "NO! MINE! MINE!" and that\'s super gremlin, like you should share more, you know? So yeah. That is, that is my opinion. I am totally happy, I am totally happy to lose viewers if it means that they find someone they love and adore watching, I really mean that. 100% okay? 100%. Listen, I do appreciate you guys, but like, I can\'t, I\'m not the kind of person whose going to hold on. - VividlyASMR April 27, 2021';
+        response = 'That is, that is my opinion. I am totally happy, I am totally happy to lose viewers if it means that they find someone they love and adore watching, I really mean that. 100% okay? 100%. - VividlyASMR';
         break;    
     default:
         response = 'Did you forget to filter? Try !quote vivi! Valid filters: ' + quotes.join(", ");
