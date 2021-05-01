2a20ebb7c033cd33a622b50f739a991fafb96558
thetruedv <matthewjdavison1+dv@outlook.com>
2021-05-01 03:45:42 +0100

    Updated mics

--- development/AriaASMR/mics.js
+++ development/AriaASMR/mics.js
@@ -2,15 +2,15 @@ var response = false;
 var micList = ['3dio', 'zoom', 'yeti'];
 switch (q.toLowerCase()) {
     case '3dio':
-        response = 'Aria is currently using her 3DIO. You can buy one here: '+
-        'https://3diosound.com/products/free-space-binaural-microphone';
+        response = 'Aria is currently using her Free Space Binaural, a mic from 3DIO. You can buy one here: '+
+        '<insert link>';
         break;
     case 'yeti':
-        response = 'Aria is currently using her Yeti, a mic from Bluemic. You can buy one here: '+
-        'https://www.bluemic.com/en-gb/products/yeti/';
+        response = 'Aria is currently using her Yeti, a mic from BlueMic. You can buy one here: '+
+        '<insert link>';
         break;
     case 'zoom':
-        response = 'Aria is currently using her H4n Pro Black, a mic from Zoomcorp. You can buy one from here: '+
+        response = 'Aria is currently using her H4n Pro Black, a mic from ZoomCorp. You can buy one from here: '+
         '<insert link>';
         break;
     default:
