9413875c35bd01dcd64181e66e4bc3d16e8a2086
thetruedv <matthewjdavison1+dv@outlook.com>
2021-05-01 03:21:21 +0100

    Updated Mics command

--- development/AriaASMR/mics.js
+++ development/AriaASMR/mics.js
@@ -1,12 +1,18 @@
 var response = false;
-var micList = ['ASMR', 'Just Chatting'];
+var micList = ['3dio', 'zoom', 'yeti'];
 switch (q.toLowerCase()) {
-    case 'asmr':
-        response = 'Aria is currently using her 3DIO';
+    case '3dio':
+        response = 'Aria is currently using her 3DIO. You can buy one here: +'
+        'https://3diosound.com/products/free-space-binaural-microphone';
         break;
-    case 'just chatting':
-        response = 'Aria is currently using her Blue Yeti';
+    case 'yeti':
+        response = 'Aria is currently using her Yeti, a mic from Bluemic. You can buy one here: +'
+        'https://www.bluemic.com/en-gb/products/yeti/';
+        break;
+    case 'zoom':
+        response = 'Aria is currently using her H4n Pro Black, a mic from Zoomcorp. You can buy one from here: '+
+        '<insert link>';
         break;
     default:
-        response = 'Did you forget to filter? Try !mic ASMR! Valid filters: ' + micList.join(", ");
+        response = 'Did you forget to filter? Try !mic 3dio! Valid filters: ' + micList.join(", ");
 }
