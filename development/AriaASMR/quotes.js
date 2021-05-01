c06daced63b5b2d4ba5e88b1ee14cf13e73174f8
thetruedv <matthewjdavison1+dv@outlook.com>
2021-05-02 00:53:15 +0100

    updated quotes

--- development/AriaASMR/quotes.js
+++ development/AriaASMR/quotes.js
@@ -2,7 +2,7 @@ var response = false;
 var quotes = ['vivi'];
 switch (q.toLowerCase()) {
     case 'vivi':
-        response = 'That is, that is my opinion. I am totally happy, I am totally happy to lose viewers if it means that they find someone they love and adore watching, I really mean that. 100% okay? 100%. - VividlyASMR';
+        response = '\'That is, that is my opinion. I am totally happy, I am totally happy to lose viewers if it means that they find someone they love and adore watching, I really mean that. 100% okay? 100%.\' - VividlyASMR. Find the full quote here: https://thetruedv.github.io/quotes?key=vivi';
         break;    
     default:
         response = 'Did you forget to filter? Try !quote vivi! Valid filters: ' + quotes.join(", ");
